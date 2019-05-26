24bde26f38b4c7a9e3b8c0e3ef52043651124f90
manhdung287 <manhdung28797@gmail.com>
2019-05-26 10:05:33 +0700

    update full

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 jQuery(document).ready(function(){
-	
+    $('#sleep').click(function(){
+        $('.win').css('opacity','0.8');
+        $('.win').css('transform','scale(1)');
+    });
+    $('.basic').click(function(){
+        $('.lose').css('opacity','0.8');
+        $('.lose').css('transform','scale(1)');
+    });
 });
 jQuery(window).resize(function(){
 	
 });
-
